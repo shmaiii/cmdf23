d5682b4f6eaae3040162614454773a4fa3277be5
aannaluo <annaluo1210@gmail.com>
2023-03-11 17:21:18 -0800

    added calendar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Calendar from 'react-calendar';
 
 function App() {
   return (
@@ -17,7 +18,14 @@ function App() {
         >
           Learn React
         </a>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
+      hello
       </header>
+      <body>
+      <Calendar/>
+      <button type="button" class="btn btn-primary">Primary</button>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      </body>
     </div>
   );
 }
