7f9ddce317cfb468549376fc5d52244d882d2a23
shmaiii <luongxuanmai2109@gmail.com>
2023-03-11 16:07:00 -0800

    backend express setup

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
