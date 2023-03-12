ab51e778b710e226ffca00bc991f02ab8fd4c8ba
aannaluo <annaluo1210@gmail.com>
2023-03-12 01:18:18 -0800

    more finalized calendar

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,12 +1,16 @@
 // import logo from './logo.svg';
-import './App.css';
+import './Home.css';
 import Calendar from 'react-calendar';
 import './calendar.css';
-import Home from './Home'
 
-function App() {
+
+
+function Home() {
+    const addImage = () => 
+        {<img src="./images/frog_bad.png" alt="bad frog" width="100" height="100"/>}
+    
   return (
-    <div className="App">
+    <div className="Home">
       <header> {/* <header className="App-header"> */}
          {/* <img src={logo} className="App-logo" alt="logo" /> */}
          {/* <p>
@@ -21,13 +25,21 @@ function App() {
         >
           Learn React
         </a> */}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"/>
       </header>
       <body>
-      <Home/>
+          Hi Emily, How are you doing today
+          <div style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center"}}>
+              <Calendar calendarType="US" next2Label="" prev2Label="" showNeighboringMonth={false} tileContent={addImage}/>
+        </div>
+      
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
       </body>
     </div>
   );
 }
 
-export default App;
+export default Home;
