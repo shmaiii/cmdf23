503281016eedae387b3a3cc5c4b1695d19f06148
shmaiii <luongxuanmai2109@gmail.com>
2023-03-12 08:08:31 -0700

    entry

--- front-end/src/pages/ViewEntry.js
+++ front-end/src/pages/ViewEntry.js
@@ -2,8 +2,12 @@ import React, {useState, useEffect} from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import "./ViewEntry.css";
+import posFrog from '../image/good-frog.png';
+import neuFrog from '../image/neutral-frog.png';
+import negFrog from '../image/bad-frog.png';
 
 const ViewEntry = (props) => {
+
     return (
         <React.Fragment>
        <Box className="text-field-container">
@@ -33,8 +37,20 @@ className="text-field-container"
 
 
 <div className="picture-options-container">
-<div className="picture-option">Based off of your diary entry, Froggy predicts that you are feeling:</div>
+{/* <div className="picture-option">Froggy analyzed that you were feeling: </div> */}
 <div className="picture-option">Your Pictures:</div>
+
+{/* <div class="row">
+<div class="column1"> */}
+<img src={props.entry.images} 
+width= "133" height= "133"/>
+{/* </div> */}
+{/* 
+<div class="column2">
+<img src={'https://images.unsplash.com/photo-1590650046871-92c887180603?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80'} 
+width= "200" height= "133"/>
+</div>
+</div> */}
 
   
 </div>
